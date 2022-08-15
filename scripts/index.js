80a52b5fedf812b6a209f404766bdc9ee63f6dfb
Emily Wetterauer <emmawetterauer@gmail.com>
2022-08-15 12:54:56 -0600

    project 4 first segment array change

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,24 @@
-const initalCards = [
+let initalCards = [
   (object1 = {
     name: "Yosemite Valley",
     link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
   }),
-
   (object2 = {
     name: "Lake Louise",
     link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
   }),
-
   (object3 = {
     name: "Bald Mountains",
     link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
   }),
-
   (object4 = {
     name: "Latemar",
     link: "https://code.s3.yandex.net/web-code/latemar.jpg",
   }),
-
   (object5 = {
     name: "Vanoise National Park",
     link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
   }),
-
   (object6 = {
     name: "Lago di Braies",
     link: "https://code.s3.yandex.net/web-code/lago.jpg",
