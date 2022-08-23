e816f39ad7758cb341ce433ae2ead1e0f8938c3d
Emily Wetterauer <emmawetterauer@gmail.com>
2022-08-23 13:17:11 -0600

    project 4 final submit with pseudo code

--- scripts/index.js
+++ scripts/index.js
@@ -25,15 +25,72 @@ const initialCards = [
   }),
 ];
 
-// EVENT LISTENERS AND HANDLERS
-
+// DOM ELEMENTS
+const cardListElement = document.querySelector(".cards__list");
 const pencilButtonElement = document.querySelector(".author__pencil");
 const popupElement = document.querySelector(".popup");
 const xButtonElement = document.querySelector(".popup__xButton");
+const profileNameElement = document.querySelector(".author__name");
+const profileDescriptionElement = document.querySelector(
+  ".author__description"
+);
+const inputDescriptionElement = document.querySelector(".popup__description");
+const inputNameElement = document.querySelector(".popup__name");
+const saveButtonElement = document.querySelector(".popup__saveButton");
+
+// Declare a getCardElement() function with one parameter named data.
+//You’ll be passing objects of the array to it. The function should:
+function getCardElement(data) {
+  // clone the template element with all its content and store it in
+  //a cardElement variable
+  const cardTemplate = document.querySelector("#cardTemplate").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  // access the card title and image and store them in variables
+  const cardTitleElement = cardElement.querySelector(".card__title");
+  const cardImageElement = cardElement.querySelector(".card__image");
+  // set the path to the image to the link field of the object
+  cardImageElement.src = data.link;
+  // set the image alt text to the name field of the object
+  cardImageElement.alt = data.name;
+  // set the card title to the name field of the object, too
+  cardTitleElement.textContent = data.name;
+  // return the ready HTML element with the filled-in data
+  return cardElement;
+}
+//Iterate over the cards array using a loop and run your new function upon each iteration to render the card one by one.
+
+for (const card of initialCards) {
+  const cardLink = card.link;
+  const cardName = card.name;
+  const cardData = {
+    name: cardName,
+    link: cardLink,
+  };
+  const cardDataElement = getCardElement(cardData);
+
+  cardListElement.append(cardDataElement);
+}
+
+// EVENT LISTENERS
+pencilButtonElement.addEventListener("click", setInputValues);
+xButtonElement.addEventListener("click", togglePopupWindow);
+saveButtonElement.addEventListener("click", saveInputValues);
+
+//EVENT HANDLERS
+function setInputValues() {
+  inputNameElement.value = profileNameElement.textContent;
+  inputDescriptionElement.value = profileDescriptionElement.textContent;
+  togglePopupWindow();
+}
 
 function togglePopupWindow() {
   popupElement.classList.toggle("popup__opened");
 }
 
-pencilButtonElement.addEventListener("click", togglePopupWindow);
-xButtonElement.addEventListener("click", togglePopupWindow);
+function saveInputValues(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = inputNameElement.value;
+  profileDescriptionElement.textContent = inputDescriptionElement.value;
+  togglePopupWindow();
+}
