8a2b0907e10d007425a94377142b113416306a90
Emily Wetterauer <emmawetterauer@gmail.com>
2022-08-23 18:44:35 -0600

    project 4 github link correction

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
 const initialCards = [
-  (object1 = {
+  {
     name: "Yosemite Valley",
     link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  }),
-  (object2 = {
+  },
+  {
     name: "Lake Louise",
     link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  }),
-  (object3 = {
+  },
+  {
     name: "Bald Mountains",
     link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  }),
-  (object4 = {
+  },
+  {
     name: "Latemar",
     link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-  }),
-  (object5 = {
+  },
+  {
     name: "Vanoise National Park",
     link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-  }),
-  (object6 = {
+  },
+  {
     name: "Lago di Braies",
     link: "https://code.s3.yandex.net/web-code/lago.jpg",
-  }),
+  },
 ];
 
 // DOM ELEMENTS
@@ -66,7 +66,7 @@ for (const card of initialCards) {
 // EVENT LISTENERS
 pencilButtonElement.addEventListener("click", setInputValues);
 xButtonElement.addEventListener("click", togglePopupWindow);
-saveButtonElement.addEventListener("click", saveInputValues);
+saveButtonElement.addEventListener("click", handleProfileFormSubmit);
 
 //EVENT HANDLERS
 function setInputValues() {
@@ -79,7 +79,7 @@ function togglePopupWindow() {
   popupElement.classList.toggle("popup__opened");
 }
 
-function saveInputValues(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileNameElement.textContent = inputNameElement.value;
   profileDescriptionElement.textContent = inputDescriptionElement.value;
